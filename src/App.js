422f5745a7291557628a269f8022e3300c64cc4a
Patty Rose <oteropatty@gmail.com>
2022-11-14 19:42:14 -0800

    add basic routing and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//BrowserRouter: connects to the browser
+//Routes: parent for all our routes
+//Route: for every page we setup a route path with two props: path, element (what we want to render)
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Error from './pages/Error';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Error = () => {
   return (
     <section className='section'>
       <h2>Error</h2>
+      <h2>404</h2>
+      <p>page not found</p>
+      <Link to='/' class='btn'>back home</Link>
     </section>
   );
 };
